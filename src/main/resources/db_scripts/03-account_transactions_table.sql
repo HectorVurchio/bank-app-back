a2e50c4c145c3f3853c52f6cb7e1112ce3df9962
Hector Vurchio <hectorvurchio@gmail.com>
2023-05-29 15:40:17 -0400

    first commit

---
+++ src/main/resources/db_scripts/03-account_transactions_table.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `account_transactions` (
+  `transaction_id` varchar(200) NOT NULL,
+  `account_number` int NOT NULL,
+  `customer_id` int NOT NULL,
+  `transaction_dt` date NOT NULL,
+  `transaction_summary` varchar(200) NOT NULL,
+  `transaction_type` varchar(100) NOT NULL,
+  `transaction_amt` int NOT NULL,
+  `closing_balance` int NOT NULL,
+  `create_dt` date DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `account_number` (`account_number`),
+  CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`account_number`) REFERENCES `accounts` (`account_number`) ON DELETE CASCADE,
+  CONSTRAINT `acct_user_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE
+);
+
+
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 7 day, 'Coffee Shop', 'Withdrawal', 30,34500,CURDATE() - INTERVAL 7 day);
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 6 day, 'Uber', 'Withdrawal', 100,34400,CURDATE() - INTERVAL 6 day);
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 5 day, 'Self Deposit', 'Deposit', 500,34900,CURDATE() - INTERVAL 5 day);
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 4 day, 'Ebay', 'Withdrawal', 600,34300,CURDATE() - INTERVAL 4 day);
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 2 day, 'OnlineTransfer', 'Deposit', 700,35000,CURDATE() - INTERVAL 2 day);
+
+INSERT INTO `account_transactions` (`transaction_id`, `account_number`, `customer_id`, `transaction_dt`, `transaction_summary`, `transaction_type`,`transaction_amt`,
+`closing_balance`, `create_dt`)  VALUES (UUID(), 1865764534, 1, CURDATE() - INTERVAL 1 day, 'Amazon.com', 'Withdrawal', 100,34900,CURDATE() - INTERVAL 1 day);
